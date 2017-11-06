744095880f3fbe384a5bcc0991fc264b88979cd7
Noel Cordova <ednoesco_7@hotmail.com>
2017-11-06 08:23:21 -0600

    Update auth.ts firebase lib

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from 'angularfire2/auth';
-import firebase from 'firebase/app';
+import firebase from 'firebase/storage';
 
 @Injectable()
 export class AuthProvider {
